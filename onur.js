4ecd1a071a16b8f119a9aaeb9de21f142911d467
ONUR DEMIRTAS <e1941939@ceng.metu.edu.tr>
2018-02-16 19:25:49 +0300

    Custom Reduce Function Implementation

--- onur.js
+++ onur.js
@@ -4,3 +4,12 @@ var map = function(arr, callback) {
         myArr.push(callback(arr[k], k, arr));
     return myArr;
 };
+
+var reduce = function(arr, callback, val) {
+    var result = (val === undefined) ? undefined : val;
+    for (var k = 0; k < arr.length; k++) {
+        if (result) result = callback.call(undefined, result, arr[k], k, arr);
+        else result = arr[k];
+    }
+    return result;
+};
